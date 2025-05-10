250fe02c6216a4aa97be999c599c1cb76a63776d
ilyaskuratovsky <ilyaskuratovsky@gmail.com>
2025-05-09 22:11:35 -0400

    filter working

--- website/people.js
+++ website/people.js
@@ -1,6 +1,58 @@
-// Patrick, David, Sam, Ilya, Kate,
-// Ally, Inga, Kaevel, Nate,
-function makeProfile(){
-  
-}
-
+const PROFILES = [
+  {
+    name: "Sam Skuratovsky",
+    photo: null,
+    quick_info: "Greenwich High School - 9th Grade",
+    about_me:
+      "This is some information about me.  I am really good at Math so I can tutor kids.  I am also on the 9th grade freshman basketball team and I've taught kids basketball.",
+    available_for: "Tutoring: Math, Spanish<br/>Sports: Basketball, Football",
+    contact: "Text me at 203-969-4261",
+    categories: ["Sports Training", "Yard work"],
+  },
+  {
+    name: "Allison Skuratovsky",
+    photo: null,
+    quick_info: "Greenwich High School - 9th Grade",
+    about_me:
+      "This is some information about me.  I am really good at Math so I can tutor kids.  I am also on the 9th grade freshman basketball team and I've taught kids basketball.",
+    available_for: "Tutoring: Math, Spanish<br/>Sports: Basketball, Football",
+    contact: "Text me at 203-969-4261",
+    categories: ["Chores", "Babysitting"],
+  },
+  {
+    name: "Kaitlyn Skuratovsky",
+    quick_info: "Greenwich High School - 9th Grade",
+    photo: null,
+    about_me:
+      "This is some information about me.  I am really good at Math so I can tutor kids.  I am also on the 9th grade freshman basketball team and I've taught kids basketball.",
+    available_for: "Tutoring: Math, Spanish<br/>Sports: Basketball, Football",
+    contact: "Text me at 203-969-4261",
+    categories: [""],
+  },
+  {
+    name: "Nate Pardo",
+    quick_info: "Greenwich High School - 9th Grade",
+    photo: null,
+    about_me:
+      "This is some information about me.  I am really good at Math so I can tutor kids.  I am also on the 9th grade freshman basketball team and I've taught kids basketball.",
+    available_for: "Tutoring: Math, Spanish<br/>Sports: Basketball, Football",
+    contact: "Text me at 203-969-4261",
+  },
+  {
+    name: "Liam Wilson",
+    quick_info: "Greenwich High School - 9th Grade",
+    photo: null,
+    about_me:
+      "Hi, my name is Liam Wilson. I'm a responsible and hardworking teen looking for part-time work in the area. I'm available to help with yard work, lawn mowing, leaf raking, and other outdoor tasks. I'm reliable, easy to work with and always do my best to get the job done right.",
+    available_for: "Yard work, lawn mowing, leaf raking, weeding",
+    contact: "Contact me at 203-724-7348 or liam.wils79@gmail.com",
+  },
+  {
+    name: "Liam Wilson",
+    quick_info: "Greenwich High School - 9th Grade",
+    photo: null,
+    about_me: "",
+    available_for: "",
+    contact: "",
+  },
+];
